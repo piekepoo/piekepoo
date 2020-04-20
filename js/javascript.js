ccb499c0a16f86b6369b705c6491a5191e01750a
piekepoo <33870920+piekepoo@users.noreply.github.com>
2020-04-20 17:56:14 +0200

    initial commit

---
+++ js/javascript.js
@@ -0,0 +1,122 @@
+javascript.js
+
+
+function onHoverAbout()
+{
+    $("#menuImg").attr('src', 'imgs/piekeblue.png');
+}
+
+function offHoverAbout()
+{
+    $("#menuImg").attr('src', 'imgs/pieke.png');
+}
+
+function onAboutClick(){
+	    $("#menuImg").attr('src', 'imgs/piekehi.png');
+}
+
+
+function onHoverTree()
+{
+    $("#menuTree").attr('src', 'imgs/christmasblue.png');
+}
+
+function offHoverTree()
+{
+    $("#menuTree").attr('src', 'imgs/christmastree.png');
+
+}
+
+function onHoverShen()
+{
+    $("#menuShen").attr('src', 'imgs/shenanigansblue.png');
+}
+
+function offHoverShen()
+{
+    $("#menuShen").attr('src', 'imgs/shenanigans.png');
+}
+
+
+function onHoverSyn()
+{
+    $("#menuSyn").attr('src', 'imgs/vinylblue.png');
+}
+
+function offHoverSyn()
+{
+    $("#menuSyn").attr('src', 'imgs/vinyl.png');
+}
+
+function onHoverAsc()
+{
+    $("#menuAsc").attr('src', 'imgs/asciiblue.png');
+}
+
+function offHoverAsc()
+{
+    $("#menuAsc").attr('src', 'imgs/ascii.png');
+}
+
+
+function onHoverSea()
+{
+    $("#menuSea").attr('src', 'imgs/seazoblue.png');
+}
+
+function offHoverSea()
+{
+    $("#menuSea").attr('src', 'imgs/seazo.png');
+}
+
+function onHoverXyz()
+{
+    $("#menuXyz").attr('src', 'imgs/xyzblue.png');
+}
+
+function offHoverXyz()
+{
+    $("#menuXyz").attr('src', 'imgs/xyz.png');
+}
+
+
+
+function onHoverIn()
+{
+    $("#menuIn").attr('src', 'imgs/linkedin.png');
+}
+
+function offHoverIn()
+{
+    $("#menuIn").attr('src', 'imgs/pieke.png');
+}
+
+function onHoverInsta()
+{
+    $("#menuInsta").attr('src', 'imgs/instagram.png');
+}
+
+function offHoverInsta()
+{
+    $("#menuInsta").attr('src', 'imgs/piekeinsta.png');
+}
+
+function onHoverFb()
+{
+    $("#menuFb").attr('src', 'imgs/facebook.png');
+}
+
+function offHoverFb()
+{
+    $("#menuFb").attr('src', 'imgs/piekefb.png');
+}
+
+function onHoverG()
+{
+    $("#menuG").attr('src', 'imgs/gmail.png');
+}
+
+function offHoverG()
+{
+    $("#menuG").attr('src', 'imgs/piekeg.png');
+}
